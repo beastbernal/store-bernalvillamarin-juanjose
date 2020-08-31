c0884b40de2ea406d2d67c6b9d07425de686b95f
jj bernal <beastbernal@gmail.com>
2020-08-31 10:35:17 -0500

    Updates

---
+++ precache-manifest.e4084a8dcfc0347aa31d724554f0b9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc5202f3705f2b896d76f9df33a6b24",
+    "url": "/store-bernalvillamarin-juanjose/index.html"
+  },
+  {
+    "revision": "2a056778d3b8cd7c04d3",
+    "url": "/store-bernalvillamarin-juanjose/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c376092411327080d0f5",
+    "url": "/store-bernalvillamarin-juanjose/static/js/2.4d6a2be2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-bernalvillamarin-juanjose/static/js/2.4d6a2be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a056778d3b8cd7c04d3",
+    "url": "/store-bernalvillamarin-juanjose/static/js/main.b23c7ee6.chunk.js"
+  },
+  {
+    "revision": "6471865fa72e2435ed7f",
+    "url": "/store-bernalvillamarin-juanjose/static/js/runtime-main.cdf14dca.js"
+  }
+]);
